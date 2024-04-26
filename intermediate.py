1da3e46440be1ec595c100e243d68dd692effe04
anasomozaa <52749830+anasomozaa@users.noreply.github.com>
2024-04-26 19:30:36 +0200

    Update intermediate.py

--- intermediate.py
+++ intermediate.py
@@ -57,7 +57,7 @@ st.write('The selected country is:', acronym_c) #calling the function to display
 def display_dataframe():
     df2 = df2[df2['Country'] == country_to_acronym(countname)]
     participants = df2.groupby(['name','shortName', 'activityType', 'organizationURL']).agg({'ecContribution':['sum']})
-    return(participants)
+    return(participants) #esto no funciona
 
 dataframe1 = display_dataframe()
 st.write(dataframe1)
